0c4351cbe02ff40539f3ad49aa38fa4e0aaa9f5c
delet-dis <efimov.036@gmail.com>
2021-12-29 14:18:20 +0700

    🗂 added basic project structure

---
+++ tasked/taskedApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskedApp.swift
+//  tasked
+//
+//  Created by Igor Efimov on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct taskedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
